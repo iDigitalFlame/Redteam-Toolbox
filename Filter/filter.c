355730922aa479e5d62d21d766e263b09d1d6a89
iDigitalFlame <idf@idigitalflame.com>
2020-01-29 10:38:24 -0500

    License Update

--- Filter/filter.c
+++ Filter/filter.c
@@ -1,4 +1,3 @@
-// iDigitalFlame 2019
 // Windows Password Filter
 // Captures Raw plaintext passwords when changed.
 // C DLL for yall scrubs.
@@ -17,6 +16,21 @@
 //  Reboot the box
 //  ??
 //  Profit!
+//
+// Copyright (C) 2020 iDigitalFlame
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 #define _WIN32_WINNT 0x0501
